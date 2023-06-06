732fba2bdadf9175ee1e2242363222ec539a64b1
tonydyatlove <106832714+tonydyatlove@users.noreply.github.com>
2023-06-06 14:54:39 +0300

    Create Moon.java

---
+++ Sun System (Singleton)/Moon.java
@@ -0,0 +1,12 @@
+public class Moon implements Planet {
+
+    private static Moon instance;
+
+    private Moon() {}
+
+    public static Moon getInstance() {
+        if (instance == null)
+            instance = new Moon();
+        return instance;
+    }
+}
